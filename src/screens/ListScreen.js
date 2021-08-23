f3d05c76050c319acce63e8e2c7ab874c88a1205
ouardaoui <ayoubouardaoui3@gmail.com>
2021-08-23 01:51:45 +0100

    just bad day

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+
+const ListScreen = () => {
+    return<Text>my screen</Text>;
+};
+const styles = StyleSheet.create({
+});
+
+export default ListScreen;
